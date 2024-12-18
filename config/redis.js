a6edb62d3e996b5e6718a03dfded8a47bddd6c5b
HussainBee Shaik <hussainbee5269@gmail.com>
2024-12-18 12:08:21 +0530

    Short Url codes

---
+++ config/redis.js
@@ -0,0 +1,18 @@
+const redis = require('redis');
+
+const client = redis.createClient({
+  host: process.env.REDIS_HOST || 'redis', 
+  port: process.env.REDIS_PORT || 6379,    
+});
+
+client.on('connect', () => {
+  console.log('Connected to Redis successfully');
+});
+
+client.on('error', (err) => {
+  console.error('Redis error:', err);
+});
+
+client.connect();  
+
+module.exports = client;
